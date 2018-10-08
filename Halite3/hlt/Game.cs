18cbb711c6feeccdc29da780e5f576e94762d90a
Jorge1o1 <jorge1o1@wharton.upenn.edu>
2018-10-08 15:59:52 -0400

    added Log

--- Halite3/hlt/Game.cs
+++ Halite3/hlt/Game.cs
@@ -5,17 +5,30 @@ using System.Text;
 
 namespace Halite3.hlt
 {
+    /// <summary>
+    /// The game object holds all metadata pertinent to the game and all its contents
+    /// </summary>
     public class Game
     {
         private int turnNumber;
+        private int myId;
 
+        /// <summary>
+        /// Initiates a game object collecting all start-state instances for the contained items for pre-game.
+        /// Also sets up basic logging.
+        /// </summary>
         public Game()
         {
             this.turnNumber = 0;
 
             //Grab constants JSON
-            string rawConstants = Console.ReadLine();
-            Constants.LoadConstants(rawConstants);
+            Constants.LoadConstants(Console.ReadLine());
+
+            string[] numPlayersAndIdStrArr = Console.ReadLine().Split();
+            int numPlayers = int.Parse(numPlayersAndIdStrArr[0]);
+            this.myId = int.Parse(numPlayersAndIdStrArr[1]);
+
+            Log.Initialize(new StreamWriter(String.Format("bot-{0}.log", myId)));
         }
     }
 }
---
+++ Halite3/hlt/Log.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace Halite3.hlt
+{
+    public class Log
+    {
+        private TextWriter file;
+        private static Log instance;
+
+        private Log(TextWriter f)
+        {
+            file = f;
+        }
+
+        public static void Initialize(TextWriter f)
+        {
+            instance = new Log(f);
+        }
+
+        public static void LogMessage(string message)
+        {
+            try
+            {
+                instance.file.WriteLine(message);
+                instance.file.Flush();
+            }
+            catch (IOException)
+            {
+            }
+        }
+    }
+}
