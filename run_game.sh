e7f32d77bd92221a3c321d53e5d206af0a41bdf0
Jorge1o1 <jorge1o1@wharton.upenn.edu>
2018-11-02 01:23:47 -0400

    Update run_game.sh

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "MyBot.exe" "MyBot.exe"
+dotnet build
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "dotnet Halite3/bin/Debug/netcoreapp2.0/MyBot.dll" "dotnet Halite3/bin/Debug/netcoreapp2.0/MyBot.dll"
